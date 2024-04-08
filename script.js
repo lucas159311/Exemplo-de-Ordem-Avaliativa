9356c5f202b7119274ca45ef86c98356cf977bd6
Lucas Avelino <50250488+lucas159311@users.noreply.github.com>
2024-04-08 12:03:57 -0300

    Atividade 7 da Lista de Exercícios de PAP

---
+++ script.js
@@ -0,0 +1,17 @@
+//inicalização de variaveis
+var a = 5;
+var b = 10;
+var c = 2;
+
+function conta1(){
+    alert(a+b*c); //exibindo resultado
+}
+
+function conta2(){
+    alert((a+b)*c); //exibindo resultado
+}
+
+function conta3(){
+    alert(a+(b*c)); //exibindo resultado
+}
+
